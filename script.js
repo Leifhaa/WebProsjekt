b21fcdf52fc09fd5e651402429a04c81c834e4b3
Eirik Andreas <eirikab@gmail.com>
2019-05-27 09:37:47 +0200

    First edition of drag&drop code

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-// Hello Gruppe 9
+/* DRAG EVENTS */
+
+var tc = document.getElementById("test-column");
+tc.setAttribute("ondrop", "drop(event)");
+tc.setAttribute("ondragover", "dropItem(event)");
+
+
+
+function dropItem(userInt) {
+    userInt.preventDefault();
+}
+
+function drag(userInt) {
+    userInt.dataTransfer.setData("text", userInt.target.id);
+}
+
+function drop(userInt) {
+    userInt.preventDefault();
+    var data = userInt.dataTransfer.getData("text");
+    userInt.target.appendChild(document.getElementById(data));
+}
+
+/* ADD CARD */
+
+var testBtn = document.getElementById("testBtn");
+testBtn.addEventListener("click", addCard);
+
+function addCard() {
+    var divHold = document.createElement('divHold');
+
+    var div = document.createElement('div');
+    div.setAttribute("id", "test-card")
+    div.setAttribute("draggable", true);
+    div.setAttribute("ondragstart", "drag(event)");
+    div.innerHTML = "Test Card";
+    document.getElementById("test-column").appendChild(div);
+
+}
+
+/* ADD COLUMN */
+
+var addCol = document.getElementById("addColumn");
+addCol.addEventListener("click", addColumn);
+
+function addColumn() {
+    var div = document.createElement('div');
+    div.setAttribute("id", "test-column-2")
+    div.setAttribute("ondrop", "drop(event)");
+    div.setAttribute("ondragover", "dropItem(event)");
+    document.getElementById("container").appendChild(div);
+
+}
