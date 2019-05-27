f8a7799ca186fa1d860bbcc9f976354fcb74042f
Eirik Andreas <eirikab@gmail.com>
2019-05-27 16:48:17 +0200

    Added delete card and delete column functionality. Dropping cards onto other cards does not merge them.

--- script.js
+++ script.js
@@ -1,43 +1,15 @@
-/* DRAG EVENTS */
 
-var tc = document.getElementById("test-column");
-tc.setAttribute("ondrop", "drop(event)");
-tc.setAttribute("ondragover", "dropItem(event)");
+// GENERATING ID FOR CARDS
+var cardArr = [];
+var testId = 0;
+var testCardName = "Test Card";
+var delId = 0;
 
+// GENERATING ID FOR COLS
+var colArr = [];
+var testColId = 0;
+var testColName = "Test Col";
 
-
-function dropItem(userInt) {
-    userInt.preventDefault();
-}
-
-function drag(userInt) {
-    userInt.dataTransfer.setData("text", userInt.target.id);
-}
-
-function drop(userInt) {
-    userInt.preventDefault();
-    var data = userInt.dataTransfer.getData("text");
-    userInt.target.appendChild(document.getElementById(data));
-}
-
-/* ADD CARD */
-
-var testBtn = document.getElementById("testBtn");
-testBtn.addEventListener("click", addCard);
-
-function addCard() {
-    var divHold = document.createElement('divHold');
-
-    var div = document.createElement('div');
-    div.setAttribute("id", "test-card")
-    div.setAttribute("draggable", true);
-    div.setAttribute("ondragstart", "drag(event)");
-    div.innerHTML = "Test Card";
-    document.getElementById("test-column").appendChild(div);
-
-
-
-}
 
 /* ADD COLUMN */
 
@@ -46,12 +18,40 @@ addCol.addEventListener("click", addColumn);
 
 function addColumn() {
     var div = document.createElement('div');
-    div.setAttribute("id", "test-column-2")
+
+    div.setAttribute("class", "column");
     div.setAttribute("ondrop", "drop(event)");
     div.setAttribute("ondragover", "dropItem(event)");
+
+    testColId += 1;
+    colArr.push({id : testColId, name : testColName });
+    for (var i = 0; i < colArr.length; i++) {
+        testCol = i;
+    }
+    console.log(colArr);
+    console.log(testCol);
+
+    div.setAttribute("id", "test-column_" + testCol);
+
+    // DELETE COL BUTTON
+    var div1 = document.createElement("div");
+    div1.setAttribute("id", "del-col");
+    div1.setAttribute("onclick", "delCol()");
+    div1.innerHTML = "x";
+
+
     document.getElementById("container").appendChild(div);
+    document.getElementById("test-column_" + testCol).appendChild(div1);
 
 }
+
+    // DELETE COL FUNCTION
+    function delCol() {
+        var deleteColumn = document.getElementById("test-column_" + testCol);
+        deleteColumn.parentNode.removeChild(deleteColumn);
+        colArr.pop(testCol);
+}
+
 
 /* EDIT COLUMN TEXT */
 
@@ -75,4 +75,78 @@ function doEdit() {
    colText.innerHTML = subject;
    colText.style.display = "inline";
    editorArea.style.display = "none";
+}
+
+
+/* ADD CARD */
+
+var testBtn = document.getElementById("testBtn");
+testBtn.addEventListener("click", addCard);
+
+function addCard() {
+    var divHold = document.createElement("divHold");
+    var div = document.createElement("div");
+
+    div.setAttribute("class", "cardStyle");
+    div.setAttribute("draggable", true);
+    div.setAttribute("ondragstart", "drag(event)");
+    div.innerHTML = testCardName;
+
+    // CREATES ID AND NAME IN cardArr
+    testId += 1;
+    cardArr.push({id : testId, name : testCardName });
+
+    for (var i = 0; i < cardArr.length; i++) {
+        delId = i;
+    }
+    console.log(delId);
+
+    div.setAttribute("id", "test-card_" + delId);
+    // PRINTS THE ARRAY
+    console.log(cardArr);
+
+    // DELETE CARD BUTTON
+    var div1 = document.createElement("div");
+    div1.setAttribute("id", "del-card");
+    div1.setAttribute("onclick", "delCard()");
+    div1.innerHTML = "x";
+
+    document.getElementById("test-column_0").appendChild(div);
+    document.getElementById("test-card_" + delId).appendChild(div1);
+
+
+}
+
+    // DELETE CARD FUNCTION
+    function delCard() {
+    var deleteCard = document.getElementById("test-card_" + delId);
+    deleteCard.parentNode.removeChild(deleteCard);
+    cardArr.pop(delId);
+
+
+}
+
+/* DRAG EVENTS */
+
+var tc = document.getElementById("test-column_" + testCol);
+tc.setAttribute("ondrop", "drop(event)");
+tc.setAttribute("ondragover", "dropItem(event)");
+
+function dropItem(userInt) {
+    userInt.preventDefault();
+
+    if (userInt.target.getAttribute("draggable") == "true")
+        userInt.dataTransfer.dropEffect = "none";
+    else
+        userInt.dataTransfer.dropEffect = "all";
+}
+
+function drag(userInt) {
+    userInt.dataTransfer.setData("text", userInt.target.id);
+}
+
+function drop(userInt) {
+    userInt.preventDefault();
+    var data = userInt.dataTransfer.getData("text");
+    userInt.target.appendChild(document.getElementById(data));
 }
