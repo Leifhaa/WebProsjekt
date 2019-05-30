e26b396e4e9880d5f20778035393019c8244fdd4
Thomas Nesset <tim98@live.no>
2019-05-30 13:21:42 +0200

    add

--- fullCard.js
+++ fullCard.js
@@ -8,14 +8,3 @@ window.onclick = function (event) {
 function changeColor(color) {
     document.getElementsByClassName('modal-content')[0].style.border = color;
 }
-
-function displayInfo() {
-    var x = document.getElementById('infoForm');
-    var text = "";
-    var i;
-    for (i = 0; i < x.length; i++) {
-        text += x.elements[i].value + "<br>";
-    }
-    document.getElementById('showInfo').innerHTML = text;
-    document.getElementById('card').style.display = 'none';
-}
