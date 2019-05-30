aa09b92e72ae5f599d1bf97ab308b7c6ee9f09dd
Leifhaa <Leifhaa@hotmail.com>
2019-05-30 15:57:20 +0200

    Abstraction of scrips

---
+++ script/TopBar.js
@@ -0,0 +1,5 @@
+var addColBtn1 = document.createElement("button");
+addColBtn1.setAttribute("id", "addColumnBtn");
+addColBtn1.setAttribute("class", "btnStyle");
+addColBtn1.innerHTML = "<i class='column fas fa-plus'></i>";
+document.getElementById("head").appendChild(addColBtn1);
