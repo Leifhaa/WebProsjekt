7e520096f23a6722c67cbe0dfd61119e68159a70
Eirik Andreas <eirikab@gmail.com>
2019-06-05 13:58:53 +0200

    Changes to board, end session btn, style settings fixed

--- script/TopBar.js
+++ script/TopBar.js
@@ -100,3 +100,22 @@ function newBoard() {
         elem.parentNode.removeChild(elem);
         elem1.parentNode.removeChild(elem1);
     }
+
+
+function endSession() {
+  document.getElementById("dropDown").classList.toggle("show");
+}
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
